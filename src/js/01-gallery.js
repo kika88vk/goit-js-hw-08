bf02c89f33f5357f75dbbea1a878a9eaf5bdc311
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-05-23 12:33:49 +0300

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map((galItem) => `<li class="gallery__item">
+<a class="gallery__link" href="${galItem.original}">
+    <img
+        class="gallery__image"
+        src="${galItem.preview}"
+        alt="${galItem.description}"
+    />
+</a>
+</li>`).join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+console.log(galleryItems);
+
+let gallerySLbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
 
 console.log(galleryItems);
