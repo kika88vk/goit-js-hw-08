218cdab956685213355ddd4d59a3e1c1de7f388b
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-05-25 14:56:21 +0300

    two tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
 
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', function(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+});
+
+const timeVideo = localStorage.getItem("videoplayer-current-time");
+const parsedTimeVideo = JSON.parse(timeVideo);
+
+
+
+player.setCurrentTime().then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+player.on("play", player.setCurrentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import "../03-feedback.html";
+const form = document.querySelector(".feedback-form");
 
+// form.addEventListener("input", input);
+form.addEventListener("submit", submit);
+
+// function input(event) {
+//     event.preventDefault();
+//     const {
+//         elements: { email, massage }
+//     } = event.currentTarget;
+//     localStorage.setItem("feedback-form-state", JSON.stringify(elements));
+// };
+
+function submit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, massage }
+    } = event.currentTarget;
+    if (email.value === "" || massage.value === "") {
+        return alert("Не заповнені поля!!!");
+    }
+    console.log({ email: email.value, massage: massage.value });
+    
+    event.currentTarget.reset();
+}
