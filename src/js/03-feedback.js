93a41d32f252352d54aa0c010fc255090d72c1ca
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-05-30 20:09:51 +0300

    Fix 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,43 +9,36 @@ import throttle from "lodash.throttle";
 form.addEventListener("input", throttle(input, 500));
 form.addEventListener("submit", submit);
 
-
-
 let formData = JSON.parse(localStorage.getItem(FEEDBACK_KEY)) || {};
+
 checkLocalStorage();
 
 function input(event) {
-    
+
     formData[event.target.name] = event.target.value;
     localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 
 };
 
-function checkLocalStorage(){
+function checkLocalStorage() {
 
-    if (formData.email) {
-        email.textContent = formData.email;
-        
-    } 
-    if (formData.message) {
-        message.textContent = formData.message;
-    } else {
-        email.textContent = "";
-        message.textContent = "";
+    if (formData) {
+        email.value = formData.email || "";
+        message.value = formData.message || "";
     }
-    
+
 }
 
 
 
 function submit(event) {
     event.preventDefault();
-    
+
     if (email.value === "" || message.value === "") {
         return alert("Не заповнені поля!!!");
     }
     console.log(formData);
-    
+
     event.currentTarget.reset();
     localStorage.removeItem(FEEDBACK_KEY);
 }
